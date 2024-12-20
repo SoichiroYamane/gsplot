40df1a43a07a05472b9f8e33365b2336b005ef9e
SoichiroYamane <SoichiroYamane@users.noreply.github.com>
2024-12-20 08:20:21 +0000

    Apply Code Formatter

--- docs/conf.py
+++ docs/conf.py
@@ -370,10 +370,8 @@ html_theme_options = {
         },
     ],
     "switcher": {
-
         "version_match": version_match,
         "json_url": json_url,
-
     },
 }
 html_static_path = ["_static"]
